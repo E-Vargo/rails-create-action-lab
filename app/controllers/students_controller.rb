22af562d1f4b075d287c078aef210cb47136ef55
E-Vargo <vargoelliott@gmail.com>
2022-01-24 16:43:38 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    s = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to student_path(s.id)
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student.id)
   end
 
 end
